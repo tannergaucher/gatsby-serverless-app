40693489f808abafe1c0ac9720d1972a5addbfa5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-09-24 10:48:18 +0800

    Add query to prisma db

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useQuery } from "@apollo/react-hooks"
+import gql from "graphql-tag"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+export default function SecondPage() {
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <PrismaDbQuery />
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
-export default SecondPage
+const MESSAGES_QUERY = gql`
+  query MESSAGES_QUERY {
+    messages {
+      text
+      id
+    }
+  }
+`
+
+function PrismaDbQuery() {
+  const { loading, error, data } = useQuery(MESSAGES_QUERY)
+
+  return (
+    <>
+      <h3>Messages from prisma db</h3>
+      {loading && `Loading messages from prisma db...`}
+      {error && `${error.message}`}
+      {data &&
+        data.messages &&
+        data.messages.map(message => (
+          <div key={message.id}>
+            <p>{message.text}</p>
+          </div>
+        ))}
+    </>
+  )
+}
