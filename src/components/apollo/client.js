61b3c3660306eaaf503651a3836c770fb12ee3e2
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-09-24 09:56:43 +0800

    fix invariant violation error: add fetch to ApolloClient

--- src/components/apollo/client.js
+++ src/components/apollo/client.js
@@ -1,5 +1,7 @@
 import ApolloClient from "apollo-boost"
+import fetch from "isomorphic-fetch"
 
 export const client = new ApolloClient({
+  fetch,
   uri: process.env.GATSBY_SITE_URL + "/.netlify/functions/graphql",
 })
