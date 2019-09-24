83ec927317bc6b6172be434d0534a77ee9e81ba9
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-09-24 10:34:22 +0800

    add client query to apollo server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
+import gql from "graphql-tag"
+import { useQuery } from "@apollo/react-hooks"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <HelloFromServer />
+      <Link to="/page-2/">Query the database on page 2</Link>
+    </Layout>
+  )
+}
 
-export default IndexPage
+const HELLO_QUERY = gql`
+  query HELLO_QUERY {
+    hello
+  }
+`
+
+function HelloFromServer() {
+  const { loading, error, data } = useQuery(HELLO_QUERY)
+
+  return (
+    <>
+      {loading && `Loading from server..`}
+      {error && `${error.message}`}
+      {data && data.hello && <p>Hello {data.hello}</p>}
+    </>
+  )
+}
